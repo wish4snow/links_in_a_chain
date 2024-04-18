d71b39886849e34f41464eef5b62aa42046fe8c8
wish4snow <157080837+wish4snow@users.noreply.github.com>
2024-04-18 12:15:41 -0500

    sort of working now

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-#include "chain_link.hpp"
-
+#include "node.hpp"
 using namespace std;
 
 int main () {
-	
+
 	ChainLink red = ChainLink("red");
+	ChainLink white = ChainLink("white");
+
+	Node one = Node(&red);
+	Node two = Node(&white, &red);
+	Node next_node = one.next();
+
+	cout << next_node.give_data().get_color();
 
 
 	return 0;
