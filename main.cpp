bf841aeb52f3d5d6521b059eda3ef5e635c3b27f
wish4snow <157080837+wish4snow@users.noreply.github.com>
2024-04-18 20:13:05 -0500

    code done and added 'better' tests

--- main.cpp
+++ main.cpp
@@ -7,16 +7,19 @@ int main () {
 	ChainLink red = ChainLink("red");
 	ChainLink white = ChainLink("white");
 	ChainLink blue = ChainLink("blue");
-	
+	ChainLink green = ChainLink("green");
+	ChainLink black = ChainLink("black");
 	
 	Node one = Node(&red);
 	Node two = Node(&white, &one);
 	Node three = Node(&blue, &two);
+	Node four = Node(&green, &three);
+	Node five = Node(&black, &four);
 
-	cout << &one << endl;
-	Node next_node = one.next();
-	cout << next_node.give_data().get_color() << endl;
-
-
+	cout << one.give_data().get_color() << endl;
+	cout << one.next().give_data().get_color() << endl;
+	cout << one.next().next().give_data().get_color() << endl;
+	cout << one.next().next().next().give_data().get_color() << endl;
+	cout << one.next().next().next().next().give_data().get_color() << endl;
 	return 0;
 }
